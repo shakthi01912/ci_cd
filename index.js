9396aa02525e6ef5f3ae490cd24f29ecef3618e0
shakthi01912 <mshakthi019@gmail.com>
2026-03-31 18:04:46 +0100

    added type  as module and support for ES6

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function add(a, b) {
-  return a + b;
-}
+import express from 'express';
+import { addtwo } from './routes/addFunction.js';
 
-module.exports = { add };
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(addtwo(2, 3).toString());
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
